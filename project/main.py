4d3c476ec43d1a81790c2195003e943c02104fe1
Eric Mattison <emattison@gmail.com>
2019-02-01 17:46:07 -0500

    added project

---
+++ project/main.py
@@ -0,0 +1,22 @@
+#!python
+# -*- coding: utf-8 -*-
+"""
+iRobot Food2Fork Application Main Executable
+
+"python main.py" runs the interactive version
+TODO: cli version
+"""
+
+
+def main():
+    print "Do thing"
+
+if __name__ == "__main__":
+    main()
+
+__author__ = "Eric Mattison"
+__maintainer__ = "Eric Mattison"
+__contact__ == "emattison@gmail.com"
+__date__ == "2019-02-01"
+__version__ == 0.1
+__status__ == "Development"
---
+++ project/template.py
@@ -0,0 +1,20 @@
+#!python
+# -*- coding: utf-8 -*-
+
+"""
+Template for creation of python files
+"""
+
+
+
+
+
+
+
+
+__author__ = "Eric Mattison"
+__maintainer__ = "Eric Mattison"
+__email__ == "emattison@gmail.com"
+__date__ == "2019-02-01"
+__version__ == 0.1
+__status__ == "Development"
