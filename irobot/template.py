c5cbe06e705f2b8349385ea44ab7eb5d46801bb4
Eric Mattison <emattison@gmail.com>
2019-02-01 21:27:53 -0500

    changed project to irobot for setuptools

--- irobot/template.py
+++ irobot/template.py
@@ -12,9 +12,8 @@ Template for creation of python files
 
 
 
+__date__ = "2019-02-01"
+__version__ = "0.0.1"
 __author__ = "Eric Mattison"
 __maintainer__ = "Eric Mattison"
-__email__ == "emattison@gmail.com"
-__date__ == "2019-02-01"
-__version__ == 0.1
-__status__ == "Development"
+__email__ = "emattison@gmail.com"
